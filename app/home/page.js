8083cb6603be5a10151880dda7f9a82d8646c0c1
Qaviashraf <126185428+Qaviashraf@users.noreply.github.com>
2024-07-07 09:27:43 -0700

    Update Home Page

--- app/home/page.js
+++ app/home/page.js
@@ -26,10 +26,10 @@ const Landing = () => {
         <div className=' leading-10 tracking-wider mb-8 p-4 md:p-20 text-center md:text-left md:mr-8'>
           <h1 
           data-aos="fade-right"
-          className='text-3xl md:text-5xl mt-10 text-yellow-600'>Welcome to<br/> Mairaj Event Planner,</h1>
+          className='text-3xl md:text-5xl mt-12 text-yellow-600'>Welcome to<br/> Mairaj Event Planner,</h1>
           <h1 
           data-aos="fade-left"
-          className='mt-6 text-xl md:text-3xl text-yellow-500 '>where dreams come to life and moments become memories.</h1> 
+          className='mt-8 text-xl md:text-3xl text-yellow-500 '>where dreams come to life and moments become memories.</h1> 
         </div>
         <div className='flex justify-center m-10'>
           <div className='group h-96 w-72'>
